1be73e0736d0e034727952f402be0080cb39bca5
Dan no Dan <dannodan89@gmail.com>
2017-07-24 21:25:06 -0400

    Cleaning a bit of code

--- src/components/key.component.js
+++ src/components/key.component.js
@@ -1,14 +1,6 @@
 import React, { Component } from 'react'
 
 class Key extends Component {
-    constructor(){
-        super()
-
-        this.state = {
-            keyValue: null
-        }
-    }
-
   render() {
     return (
       <button className="key" onClick={() => this.props.onKeyPress()}>
